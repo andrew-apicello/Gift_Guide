086625d6915866b8532007f5c8700a838a175c1d
Andrew Apicello <andrew.apicello@gmail.com>
2017-09-08 14:49:04 -0400

    Initialize Website

---
+++ Clothes/G3_CLOTHES_JS.js
@@ -0,0 +1,8 @@
+
+var person = {
+  name: "Mike",
+  age: 25,
+  favoriteFood: "pizza"
+};
+
+console.log("My best friend's name is" + person.name + ",he's " + person.age + " years old and his favorite food is " + person.favoriteFood);
